da36955ac66fe59f9f18c2ad221bb99d6c39f5c7
Kenneth Díaz <kennethdiazgonzalez@hotmail.com>
2024-10-29 17:08:26 -0500

    Added changes to FastAPI docs

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Traviak API",
+    description="API for Traviak",
+    version="0.0.1",
+    contact={
+        "name": "Traviak",
+        "url": "https://traviak.com",
+        "email": "info@traviak.com"
+    }
+
+)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Message": "Welcome to Traviak API"}
